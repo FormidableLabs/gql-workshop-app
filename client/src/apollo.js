493bba2df96ef6cdd56e9e1239c185783356eef9
Imran Sulemanji <imranolas@gmail.com>
2018-02-28 17:51:37 +0000

    Create an auth link
    To set Authorization headers for each request

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,11 +1,29 @@
 import { ApolloClient } from 'apollo-client';
 import { BatchHttpLink } from 'apollo-link-batch-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+
+const authLink = new ApolloLink((operation, forward) => {
+  const context = operation.getContext()
+  const nextContext = {
+    ...context,
+    headers: {
+      ...context.headers,
+      authorization: localStorage.getItem('token')
+    }
+  }
+  operation.setContext(nextContext);
+
+  return forward(operation);
+})
 
 const client = new ApolloClient({
-  link: new BatchHttpLink({
-    uri: 'http://localhost:3001/graphql'
-  }),
+  link: ApolloLink.from([
+    authLink,
+    new BatchHttpLink({
+      uri: 'http://localhost:3001/graphql'
+    })
+  ]),
   cache: new InMemoryCache()
 });
 
