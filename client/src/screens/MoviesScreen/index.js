4f69b6ea33a998cbc4271c63f3dcf6a395b912a6
Imran Sulemanji <imranolas@gmail.com>
2018-02-28 17:37:09 +0000

    Create Movies query

--- client/src/screens/MoviesScreen/index.js
+++ client/src/screens/MoviesScreen/index.js
@@ -15,7 +15,7 @@ const FavCount = ({ count }) => {
   </div>
 }
 
-const Movies = ({ movies, favorites, loading, loadMore }) => {
+const Movies = ({ movies, favorites = [], loading, loadMore }) => {
   if (loading) return null;
 
   return (
@@ -43,28 +43,27 @@ const Movies = ({ movies, favorites, loading, loadMore }) => {
  *   - Add pagination using FetchMore
  */
 
-const withData = withProps(() => ({
-  movies: [{
-    "id": "269149",
-    "title": "Zootopia",
-    "posterPath": "https://image.tmdb.org/t/p/w500/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
-    "releaseDate": "2016-02-11",
-    "overview": "Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia's police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.",
-    "isFavorite": true
-  },
+const withData = graphql(
+  gql`
+    query Movies($page: Int) {
+        movies(page: $page) {
+          id
+          title
+          posterPath(size: MEDIUM)
+          releaseDate
+          overview
+          isFavorite
+        }
+    }
+  `,
   {
-    "id": "284054",
-    "title": "Black Panther",
-    "posterPath": "https://image.tmdb.org/t/p/w500/bLBUCtMQGJclH36clliPLmljMys.jpg",
-    "releaseDate": "2018-02-13",
-    "overview": "After the events of Captain America: Civil War, King T'Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K.",
-    "isFavorite": false
-  }],
-  favorites: [{
-    "id": "269149",
-    "isFavorite": true
-  }],
-  loading: false
-}));
+    props: ({ data: { movies, loading } }) => {
+      return {
+        movies,
+        loading
+      }
+    }
+  }
+);
 
 export default withData(Movies);
