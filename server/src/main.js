49fb9bd369a279f01a42f2387030e14d0f706025
Imran Sulemanji <imranolas@gmail.com>
2018-02-28 14:00:00 +0000

    Add mock authentication
    We can authenticate via the GQL and will receive a mock token. When we post this token in the Authorization header, our GQL service will flag us as `loggedIn`.

--- server/src/main.js
+++ server/src/main.js
@@ -4,12 +4,28 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const isemail = require('isemail');
 
 app.use(cors());
 
 const { schema, context } = require('./schema');
 
 const PORT = 3001;
+
+/**
+ *  Mock Authentication Middleware
+ */
+app.use((req, res, next) => {
+  const auth = req.headers.authorization || '';
+  const email = new Buffer(auth, 'base64').toString('ascii');
+
+  if (isemail.validate(email)) {
+    req.email = email
+  }
+
+  return next();
+});
+
 
 app.use(
   '/graphql',
