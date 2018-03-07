8ad39a0d029e459b0935a53ae269debb6bf04d7a
Peggy Rayzis <peggyrayzis@gmail.com>
2018-03-07 19:10:21 +0000

    setContext takes a function

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -3,15 +3,12 @@ import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
   request: operation => {
-    const context = operation.getContext();
-    const nextContext = {
-      ...context,
+    operation.setContext(context => ({
       headers: {
         ...context.headers,
         authorization: localStorage.getItem('token'),
       },
-    };
-    operation.setContext(nextContext);
+    }));
   },
 });
 
